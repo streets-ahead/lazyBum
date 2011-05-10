9934808378c22d8720a3f38ee7960cd0d12856b1
Sam Mussell <sam@streetsaheadllc.com>
2011-05-09 19:34:28 -0600

    added paging module

---
+++ tests/paginTest.js
@@ -0,0 +1,14 @@
+var paging = require('../lib/helpers/paging');
+
+var p = paging.createPagination(['one', 'two', 'three', 'four', 'five'], '/articles', '');
+p.setPageSize(2);
+console.log(p.createLinks());
+
+console.log(p.getCurrentResults());
+
+
+p.currPage = 1;
+console.log(p.getCurrentResults());
+
+p.currPage = 2;
+console.log(p.getCurrentResults());
