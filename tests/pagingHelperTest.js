6975beba13c229283341faaf00b45313fdbad9dc
Sam Mussell <sam@streetsaheadllc.com>
2011-07-23 16:15:58 -0600

    fixed the paging helper and completed test

--- tests/pagingHelperTest.js
+++ tests/pagingHelperTest.js
@@ -11,26 +11,27 @@ pagingHelperTest.prototype.setUp = function() {
 }
 
 pagingHelperTest.prototype.testCreateLinks = function() {
-	var expected = '<p><a href="/articles?currentPage=0">&lt;&lt;&nbsp;&nbsp;First</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=0">&lt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="/articles?currentPage=0">0</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=1">1</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=2">2</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=1">&gt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=2">Last&nbsp;&nbsp;&gt;&gt;</a></p>';
-	var actual = this.p.createLinks();
-	assert.equal(expected, actual, 'links did not match \n ' + actual);
-	
-	this.p.currPage = 2;
-	expected = '<p><a href="/articles?currentPage=0">&lt;&lt;&nbsp;&nbsp;First</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=0">&lt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=0">0</a>&nbsp;&nbsp;&nbsp;&nbsp;<strong><a href="/articles?currentPage=1">1</a></strong>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=2">2</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=1">&gt;</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/articles?currentPage=2">Last&nbsp;&nbsp;&gt;&gt;</a></p>';
-	actual = this.p.createLinks();
-	assert.equal(expected, actual, 'links did not match for test 2 \n ' + actual);
+
+	var expected = '<ol><li class="currentPage"><a href="/articles?currentPage=1">1</a></li><li><a href="/articles?currentPage=2">2</a></li><li><a href="/articles?currentPage=3">3</a></li><li class="nextPageLink"><a href="/articles?currentPage=2">&gt;</a></li><li class="lastPageLink"><a href="/articles?currentPage=3">Last&nbsp;&nbsp;&gt;&gt;</a></li></ol>';
+		var actual = this.p.createLinks();
+		assert.equal(expected, actual, 'links did not match \n ' + actual);
+		
+		this.p.currPage = 3;
+		expected = '<ol><li class="firstPageLink"><a href="/articles?currentPage=1">&lt;&lt;&nbsp;&nbsp;First</a></li><li class="prevPageLink"><a href="/articles?currentPage=2">&lt;</a></li><li><a href="/articles?currentPage=1">1</a></li><li><a href="/articles?currentPage=2">2</a></li><li class="currentPage"><a href="/articles?currentPage=3">3</a></li></ol>';
+		actual = this.p.createLinks();
+		assert.equal(expected, actual, 'links did not match for test 2 \n ' + actual);
 }
 
 pagingHelperTest.prototype.testGetCurrentRestuls = function() {
 	var expected = [ 'one', 'two', 'three' ];
 	var actual = this.p.getCurrentResults();
-	assert.equal(expected, actual, 'did not match ' + actual);
-
-
+	assert.deepEqual(expected, actual, 'did not match ' + actual);
+	
+	
 	expected = [ 'four', 'five', 'six' ];
 	this.p.currPage = 2;
 	actual = this.p.getCurrentResults();
-	assert.equal(expected, actual, 'did not match ' + actual);
+	assert.deepEqual(expected, actual, 'did not match ' + actual);
 }
 
 module.exports = pagingHelperTest;
