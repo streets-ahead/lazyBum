63904b9c9453efdcff8478ad0fb18d2985683f2a
Sam Mussell <sam@streetsaheadllc.com>
2011-10-07 23:35:08 -0600

    removes unused routeHandler feature and cleaned up config template

--- app_template/config.js
+++ app_template/config.js
@@ -1,35 +1,16 @@
 module.exports = {
 	"baseURL" : "http://localhost",
 	"port" : 8888,
-	"defaultRenderer" : "html",
 	"routeMaps" : [
 		{
 		 	"path" : "/",
 		 	"dest" : "/index"
 	 	}
 	],
-	"routeHandlers" : [
-		
-	],
-	"MIME_TYPES" : {
-		"eot" : "application/vnd.ms-fontobject",
-		"ttf" : "font/ttf",
-		"otf" : "font/otf",
-		"ico" : "image/x-icon",
-		"css" : "text/css",
-		"js"  : "application/x-javascript",
-		"xml" : "application/xml"
-	},
 	"preControllerHooks" : [
 		// "authenticate"
 	],
 	"postControllerHooks" : [
-	],
-	"secureRoutes":[
-		{
-			//"route" : "/article/new",
-			//"methods" : ['get']
-		}
 	],
 	"databaseName" : "streetsahead",
 	"logAppenders" : [
@@ -45,11 +26,5 @@ module.exports = {
 			"file" : "console",
 			"level" : "trace"
 		}
-	],
-	"templateCache" : 3600, // in seconds
-	"templateTags" : {
-		"start" : "<%",
-		"end" : "%>"
-	},
-	"mobileEnabled" : false
+	]
 };
